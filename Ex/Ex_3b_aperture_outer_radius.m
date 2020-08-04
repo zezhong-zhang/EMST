0fc5fcf77f9834b156515049ba11c193cb80bc24
zezhong-zhang <jack.zezhong.zhang@gmail.com>
2020-08-04 12:53:32 +0800

    tutorial 1

---
+++ Ex/Ex_3b_aperture_outer_radius.m
@@ -0,0 +1,60 @@
+% output_multislice = il_multem(system_conf, input_multem) perform TEM simulation
+% Incident wave simulation
+% All parameters of the input_multem structure are explained in ilm_dflt_input_multem()
+% Copyright 2020 Ivan Lobato <Ivanlh20@gmail.com>
+
+clear; clc;
+addpath([fileparts(pwd) filesep 'mex_bin'])
+addpath([fileparts(pwd) filesep 'crystalline_materials'])
+addpath([fileparts(pwd) filesep 'matlab_functions'])
+
+input_multem = ilm_dflt_input_multem();         % Load default values;
+
+system_conf.precision = 1;                     % eP_Float = 1, eP_double = 2
+system_conf.device = 2;                        % eD_CPU = 1, eD_GPU = 2
+system_conf.cpu_nthread = 4; 
+system_conf.gpu_device = 0;
+
+input_multem.E_0 = 300;                          % Acceleration Voltage (keV)
+input_multem.theta = 0.0;
+input_multem.phi = 0.0;
+
+input_multem.spec_lx = 20;
+input_multem.spec_ly = 20;
+
+input_multem.nx = 1024; 
+input_multem.ny = 1024;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%% Incident wave %%%%%%%%%%%%%%%%%%%%%%%%%%
+input_multem.iw_type = 2;   % 1: Plane_Wave, 2: Convergent_wave, 3:User_Define, 4: auto
+input_multem.iw_psi = read_psi_0_multem(input_multem.nx, input_multem.ny);    % user define incident wave
+input_multem.iw_x = 0.5*input_multem.spec_lx;    % x position 
+input_multem.iw_y = 0.5*input_multem.spec_ly;    % y position
+
+%%%%%%%%%%%%%%%%%%%%%%%% condenser lens %%%%%%%%%%%%%%%%%%%%%%%%
+input_multem.cond_lens_m = 0;                  % Vortex momentum
+input_multem.cond_lens_c_10 = 0;          % Defocus (�)
+input_multem.cond_lens_c_30 = 0;            % Third order spherical aberration (mm)
+input_multem.cond_lens_c_50 = 0.00;             % Fifth order spherical aberration (mm)
+input_multem.cond_lens_c_12 = 0.0;              % Twofold astigmatism (�)
+input_multem.cond_lens_phi_12 = 0.0;          % Azimuthal angle of the twofold astigmatism (�)
+input_multem.cond_lens_c_23 = 0.0;              % Threefold astigmatism (�)
+input_multem.cond_lens_phi_23 = 0.0;          % Azimuthal angle of the threefold astigmatism (�)
+input_multem.cond_lens_inner_aper_ang = 18;   % Inner aperture (mrad) 
+input_multem.cond_lens_outer_aper_ang = 21.0;  % Outer aperture (mrad)
+input_multem.cond_lens_ti_sigma = 0;                % standard deviation (�)
+input_multem.cond_lens_ti_npts = 0;               % # of integration points. It will be only used if illumination_model=4
+input_multem.cond_lens_si_sigma = 0.2;             % standard deviation: For parallel ilumination(�^-1); otherwise (�)
+input_multem.cond_lens_si_rad_npts = 8;             % # of integration points. It will be only used if illumination_model=4
+input_multem.cond_lens_zero_defocus_type = 1;  % eZDT_First = 1, eZDT_User_Define = 4
+input_multem.cond_lens_zero_defocus_plane = 0;
+
+
+
+output_incident_wave = il_incident_wave(system_conf, input_multem);
+psi_0 = output_incident_wave.psi_0;
+probe = abs(psi_0).^2;
+
+imagesc(probe);
+colormap(gray)
+axis tight equal;
---
+++ README.md
@@ -0,0 +1,46 @@
+# Electron Microscopy Simulation Tutorial (EMST)
+
+This is a repository that holds a series of tutorials for theoritical and practical aspect of electron microscopy simulations, aiming to support young researchers in electron microscopy. 
+## Software 
+
+MULTEM: https://github.com/Ivanlh20/MULTEM
+
+StatSTEM: https://github.com/quantitativeTEM/StatSTEM
+
+## Development Plan
+
+1. Basic electron scattering and introduction to MULTEM [x]
+(Oxford, 5 Augest 2020)
+    - Electron wave
+    - Electron scattering using Schrondinger equation
+    - Multislice representation
+    - Sampling
+    - Hand-on MULTEM
+
+2. Quantitative scanning transmission electron microscopy
+    - Experiment
+        - characetrise the convergence angle and collection angle 
+        - characterise your detector response
+    - Quntification 
+        - Peak refinement 
+        - scattering cross-section 
+            - Voronoi cell intergration
+            - Gaussian integration
+        - Atom counting
+            - Statistical method
+            - Simulation-based scattering cross-section library 
+        - Strain mapping
+    - Hand-on StatSTEM
+
+3. Kinematic and dynamic electron scattering
+
+4. Elastic and inelastic electron scattering 
+
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
